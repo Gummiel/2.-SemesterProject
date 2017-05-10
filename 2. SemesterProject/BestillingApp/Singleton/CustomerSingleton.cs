fd5a74a47806b7c7896f0eeab1aa20344aeb706f
Gummiel <mikael.roswall@hotmail.com>
2017-05-10 10:25:27 +0200

    CustomerSingleton

--- 2. SemesterProject/BestillingApp/Singleton/CustomerSingleton.cs
+++ 2. SemesterProject/BestillingApp/Singleton/CustomerSingleton.cs
@@ -2,6 +2,7 @@
 
 using System.Collections.ObjectModel;
 using BestillingApp.Model;
+using BestillingApp.Persistency;
 
 #endregion
 
@@ -28,6 +29,47 @@ namespace BestillingApp.Singleton
         public static CustomerSingleton Instance => _instance ?? (_instance = new CustomerSingleton());
 
         public ObservableCollection<Customer> Customers = new ObservableCollection<Customer>();
+
+        #endregion
+
+        #region LoadCustomerAsync
+
+        public async void LoadCustomersAsync()
+        {
+            var customers = await PersistencyService.LoadCustomersFromJsonAsync();
+            if (customers != null)
+            {
+                foreach (var cust in customers)
+                {
+                    customers.Add(cust);
+                }
+            }
+            else
+            {
+                //possibly exception
+            }
+        }
+
+        #endregion
+
+        #region Add
+
+        public void AddCustomer(int id, string name, string email, string address, int telNo, int zipcode, string city)
+        {
+            Customer newCustomer = new Customer(id, name, email, address, telNo, zipcode, city);
+            Customers.Add(newCustomer);
+            PersistencyService.SaveCustomerAsJsonAsync(newCustomer);
+        }
+
+        #endregion
+
+        #region Remove
+
+        public void RemoveCustomer (Customer c)
+        {
+            Customers.Remove(c);
+            PersistencyService.DeleteCustomerAsync(c);
+        }
 
         #endregion
     }
