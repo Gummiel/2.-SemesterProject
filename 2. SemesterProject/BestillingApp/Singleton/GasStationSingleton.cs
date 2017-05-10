714780469efab86dfef2194295a3f78b10b9064f
Gummiel <mikael.roswall@hotmail.com>
2017-05-10 11:13:31 +0200

    Order og GasStation Singletons

--- 2. SemesterProject/BestillingApp/Singleton/GasStationSingleton.cs
+++ 2. SemesterProject/BestillingApp/Singleton/GasStationSingleton.cs
@@ -2,6 +2,7 @@
 
 using System.Collections.ObjectModel;
 using BestillingApp.Model;
+using BestillingApp.Persistency;
 
 #endregion
 
@@ -28,6 +29,48 @@ namespace BestillingApp.Singleton
         public static GasStationSingleton Instance => _instance ?? (_instance = new GasStationSingleton());
 
         public ObservableCollection<GasStation> GasStations = new ObservableCollection<GasStation>();
+
+        #endregion
+
+        #region LoadGasStationAsync
+
+        public async void LoadGasStationAsync()
+        {
+            var gasstations = await PersistencyService.LoadGasStationsFromJsonAsync();
+            if (gasstations != null)
+            {
+                foreach (var gas in gasstations)
+                {
+                    gasstations.Add(gas);
+                }
+            }
+            else
+            {
+                //possibly exception
+            }
+        }
+
+        #endregion
+
+        #region Add
+
+        public void AddGasStation(int id, string name, string address, string city, int zipcode, string email, int telNo,
+            string openHours)
+        {
+            GasStation newGasStation = new GasStation(id, name, address, city, zipcode, email, telNo, openHours);
+            GasStations.Add(newGasStation);
+            PersistencyService.SaveGasStationAsJsonAsync(newGasStation);
+        }
+
+        #endregion
+
+        #region Remove
+
+        public void RemoveGasStation(GasStation g)
+        {
+            GasStations.Remove(g);
+            PersistencyService.DeleteGasStationAsync(g);
+        }
 
         #endregion
     }
--- 2. SemesterProject/BestillingApp/Singleton/OrderSingleton.cs
+++ 2. SemesterProject/BestillingApp/Singleton/OrderSingleton.cs
@@ -2,6 +2,7 @@
 
 using System.Collections.ObjectModel;
 using BestillingApp.Model;
+using BestillingApp.Persistency;
 
 #endregion
 
@@ -23,19 +24,46 @@ namespace BestillingApp.Singleton
 
         #endregion
 
-        #region Add
-
-        public void AddOrder(int orderID)
-        {
-        }
-
-        #endregion
-
         #region Properties
 
         public static OrderSingleton Instance => _instance ?? (_instance = new OrderSingleton());
 
         public ObservableCollection<Order> Orders = new ObservableCollection<Order>();
+
+        #endregion
+
+        #region LoadOrderAsync
+
+        public async void LoadOrderAsync()
+        {
+            var orders = await PersistencyService.LoadOrdersFromJsonAsync();
+            if (orders != null)
+            {
+                foreach (var ord in orders)
+                {
+                    orders.Add(ord);
+                }
+            }
+        }
+
+        #region Add
+
+        public void AddOrder(int id, int totalPrice)
+        {
+            Order newOrder = new Order(id, totalPrice);
+            Orders.Add(newOrder);
+            PersistencyService.SaveOrderAsJsonAsync(newOrder);
+        }
+
+        #endregion
+
+        #region Remove
+
+        public void RemoveOrder(Order o)
+        {
+            Orders.Remove(o);
+            PersistencyService.DeleteOrderAsync(o);
+        }
 
         #endregion
     }
